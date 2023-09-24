a56f30fe8cb8cf6d52214075edad21217f81ce2b
Evan <suzukaze.hazuki2020@gmail.com>
2023-09-24 13:08:06 +0900

    add dev-tools

--- src/pages/cart/index.tsx
+++ src/pages/cart/index.tsx
@@ -1,14 +1,29 @@
 import { View } from '@tarojs/components'
+import { useCountStore } from '@/stores/count'
+import { Button } from "@nutui/nutui-react-taro"
 import './index.scss'
 
 function Index() {
   return (
     <View className="nutui-react-demo">
-      <View className="index">
-        Cart
-      </View>
+      <Counter />
+      <Controls />
     </View>
   )
+}
+
+function Counter() {
+  const bears = useCountStore((state: any) => state.bears)
+  return <h1>{bears} around here ...</h1>
+}
+
+function Controls() {
+  const increasePopulation = useCountStore((state: any) => state.increasePopulation)
+  return <View className="index">
+    <Button onClick={increasePopulation} type="primary" className="btn">
+      +1
+    </Button>
+  </View>
 }
 
 export default Index
--- src/stores/count.ts
+++ src/stores/count.ts
@@ -2,7 +2,7 @@ import { create } from 'zustand'
 import { mountStoreDevtool } from 'simple-zustand-devtools';
 
 
-export const useBearStore = create((set) => ({
+export const useCountStore = create((set) => ({
   bears: 0,
   increasePopulation: () => set((state) => ({ bears: state.bears + 1 })),
   removeAllBears: () => set({ bears: 0 }),
@@ -10,5 +10,5 @@ export const useBearStore = create((set) => ({
 
 
 if (process.env.NODE_ENV === 'development') {
-  mountStoreDevtool('bearStore', useBearStore);
+  mountStoreDevtool('count', useCountStore);
 }
