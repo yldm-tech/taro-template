4a8667191900de24449eb08b89128eacf3575c28
Evan <suzukaze.hazuki2020@gmail.com>
2023-09-25 10:56:51 +0900

    fix tailwindcss

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,11 @@
 // 全局样式
-import './app.css'
+import '@/assets/css/tailwind.css'
 
 import { useDidHide,useDidShow } from '@tarojs/taro'
 import { ReactElement, useEffect } from 'react'
 
 
-function App (props: { children: ReactElement }) {
+const App =(props: { children: ReactElement }) =>{
   // 可以使用所有的 React Hooks
   useEffect(() => {})
 
